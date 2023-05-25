3525807d04187bb5f020e619f860e011a682b712
mozamel-hmd <99012621+mozamel-hmd@users.noreply.github.com>
2023-05-25 19:48:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Login page </title>
+</head>
+<body>
+	<form action="#">
+	<div class="input-box">
+	<div class="square">
+		
+		<div class="login">
+			<h2>Login</h2>
+			<div class="inputBx">
+				<input type="text" placeholder="Pseudo" name="Pseudo">
+			</div>
+			<div class="inputBx">
+				<input type="password" placeholder="Password" name="Pass">
+			</div>
+			<div class="inputBx">
+				<input type="submit" value="Sign in">
+			</div>
+			<div class="links">
+				<a href="#">Forget Password</a>
+				<a href="#">Signup</a>
+				</form>
+			</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
