5d8a53d3cc838e06cc434faea35a575fa86c7296
mozamel-hmd <99012621+mozamel-hmd@users.noreply.github.com>
2023-05-25 19:47:31 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #D0C2C2;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a href="#home">Logo</a></li>
+ 
+ 
+</ul>
+
+</body>
+</html>
